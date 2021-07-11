43595109eaf73172612a145c92a895173bbff909
naeginn <send.to.naegin@gmail.com>
2021-07-11 18:00:47 -0300

    - Objetos
    00:23:50

---
+++ files/js/aula_5.js
@@ -0,0 +1,16 @@
+// 5° - Objetos.
+
+// Anotações
+// Um objeto sempre vem acompanhado da key e value.
+// let object = {key:value};
+
+let pessoa = {
+    nome: 'Naegin',
+    idade: 25,
+    estarAprovado: true,
+    sobrenome: null,
+    corSelecionado: undefined
+};
+
+console.log(pessoa);
+console.log(pessoa.nome);

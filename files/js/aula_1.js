ad68e997958746be084be1461c75f2dd4b2b6aa4
naeginn <send.to.naegin@gmail.com>
2021-07-11 17:30:02 -0300

    - Variáveis
    00:07:36

---
+++ files/js/aula_1.js
@@ -0,0 +1,16 @@
+// 1° - Variavel.
+
+// Anotações
+
+// Não é possivel começar o nome de uma variavel com um número.
+// Não é possivel dar espaço no nome de uma variavel.
+// Últilizar camelCase pra boas praticas.
+// Javascript tem casesenstive.
+
+let idade = 25;
+let nome = 'Naegin';
+let pais = 'Portugal'
+
+console.log(idade);
+console.log(nome);
+console.log(pais)

6d51499dce5e7bf5cb00003e505658f6cf1c51d2
naeginn <send.to.naegin@gmail.com>
2021-07-11 17:53:21 -0300

    - Tipagem Dinâmica no Javascript
    00:19:08

---
+++ files/js/aula_4.js
@@ -0,0 +1,16 @@
+// 4° - Tipagem Dinâmica no Javascript.
+
+// Anotações
+// Como identificar um valor de um variavel..
+
+let nome = 'Naegin';
+let idade = 25;
+let estarAprovado = true;
+let sobrenome = undefined;
+let corSelecionado = null;
+
+console.log(typeof(idade));
+
+idade = false;
+
+console.log(typeof(idade));

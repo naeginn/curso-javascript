cbaebf83b0d15b7376e0dca041b5508562bef3e7
naeginn <send.to.naegin@gmail.com>
2021-07-11 17:43:58 -0300

    - Tipos Primitivos
    00:14:29

---
+++ files/js/aula_3.js
@@ -0,0 +1,16 @@
+// 3° - Tipos Primitivos.
+
+// Anotações
+
+// Os tipos (tipo de referencia) e (tipo de valores)
+
+// String literal (quando o valor estar dentro de aspas)
+let nome = 'Naegin';
+// Number literal (quando o valor é um numeral)
+let idade = 25;
+// Booleano (quando o valor é true ou false)
+let estarAprovado = true;
+// Variavel sem definição
+let sobrenome = undefined;
+// Redefinir um valor.
+let corSelecionado = null;

8f87c52ab94bc5a24b500f4369ba94ba7250b067
naeginn <send.to.naegin@gmail.com>
2021-07-11 17:35:12 -0300

    - Constantes
    00:12:40

---
+++ files/js/aula_2.js
@@ -0,0 +1,10 @@
+// 2° - Variavel.
+
+// Anotações
+
+// Não é possivel alterar valores de um const.
+// Recomendavél usar let no lugar do const quando o valor sofre alteração constantemente.
+
+const idade = 20;
+
+console.log(idade);

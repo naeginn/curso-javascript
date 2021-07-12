ed3fb2a07e24c60c1be389d0bc3383a44b799d24
naeginn <send.to.naegin@gmail.com>
2021-07-12 05:55:26 -0300

    - Arrays
    00:28:19

---
+++ files/js/aula_6.js
@@ -0,0 +1,11 @@
+// 6° - Arrays.
+
+// Anotações
+// Array são conjuntos de dados.
+// Index é a posição exata de um conjunto.
+
+let numeros = ['gg', 2, true, 4];
+
+console.log(numeros);
+console.log(numeros.length);
+console.log(numeros[2])

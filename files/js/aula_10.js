fb2b8c63cbf0385b964c4eb3674d47b14bed3b55
naeginn <send.to.naegin@gmail.com>
2021-07-12 07:46:51 -0300

    - Operadores aritimeticos
    00:43:28

---
+++ files/js/aula_10.js
@@ -0,0 +1,24 @@
+// 8° - Operadores aritimeticos.
+
+// Anotações
+// Somar = +
+// Subtrair = -
+// Multiplicação = *
+// Divisão = /
+// Raiz Quadra = **
+
+// Imcremento = ++
+// Decremento = --
+// so mostra o valor Incre/Decre depois da função executada.
+
+let salario = 5;
+let salarioDecre = salario--;
+let salarioIncre = salario++;
+
+console.log(salario + salario);
+console.log(salario - salario);
+console.log(salario * salario);
+console.log(salario / salario);
+console.log(salario ** salario);
+console.log(salarioIncre);
+console.log(salarioDecre);

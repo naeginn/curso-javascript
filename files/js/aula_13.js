7ee1862fa78b6d6ef4bc247689c14d849dccbc79
naeginn <send.to.naegin@gmail.com>
2021-07-12 17:41:15 -0300

    - Operador Ternário
    00:52:08

---
+++ files/js/aula_13.js
@@ -0,0 +1,9 @@
+// 13° - Operador Ternário.
+
+// Anotações
+// o primeiro será true e o segundo será false.
+// let nome = comparação ? 'true': 'false'
+
+let pontos = 100;
+let tipo = pontos > 100 ? 'premium' : 'comum';
+console.log(tipo);

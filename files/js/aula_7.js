1263c26458f9cc3b334b96a824393bf121524c7e
naeginn <send.to.naegin@gmail.com>
2021-07-12 06:43:26 -0300

    - Functions
    00:32:35

---
+++ files/js/aula_7.js
@@ -0,0 +1,13 @@
+// 7° - Functions.
+
+// Anotações
+// Verbo + Substantivo pra nomear um nome de uma função
+let nome = 'Naegin';
+
+function alterarNome(name, sobrenome){
+    nome = name + ' ' + sobrenome
+};
+
+console.log(nome);
+alterarNome('Daniel', 'Araujo');
+console.log(nome)

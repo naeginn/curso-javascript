ccc13172ee91806cae575da63e8561f72305b1cf
naeginn <send.to.naegin@gmail.com>
2021-07-12 06:53:12 -0300

    - Tipos de Funções
    00:38:54

---
+++ files/js/aula_8.js
@@ -0,0 +1,19 @@
+// 8° - Functions.
+
+// Anotações
+
+// Função que executar algo.
+function dizerNome(nome){
+    console.log(nome);
+};
+
+dizerNome('Naegin');
+
+// Função que retorna algo.
+function multiplicar(numeroUm, numeroDois){
+    return numeroUm * numeroDois
+};
+
+let resultado = multiplicar(2, 2);
+console.log(resultado);
+

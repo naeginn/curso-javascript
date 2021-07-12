d3f18973837272e3677d43de2c232f9dc229ab72
naeginn <send.to.naegin@gmail.com>
2021-07-12 16:38:09 -0300

    - Operadores de igualdade
    00:49:13

---
+++ files/js/aula_12.js
@@ -0,0 +1,12 @@
+// 12° - Operadores de igualdade.
+
+// Anotações
+// Igualdade solta comparar os valores e conveter pro tipo do principal.
+// Igualdade estrita compara os tipos, valores.
+
+// === Igualdade estrita
+console.log(1 === 1);
+
+// == Igualdade solta.
+console.log('1' == 1);
+

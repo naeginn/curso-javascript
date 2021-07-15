2c2fbc1278e751a28d8e4ae00d5b7739508be5b0
naeginn <send.to.naegin@gmail.com>
2021-07-15 11:07:14 -0300

    - If..Else
    01:08:42

---
+++ files/js/aula_17.js
@@ -0,0 +1,12 @@
+
+let hora = 6;
+
+if (hora > 6 && hora < 12){
+	console.log('Bom dia.');
+}
+else if (hora > 12 && hora < 18){
+	console.log('boa tarde');
+
+} else {
+  console.log('Boa noite.')
+}

34e1ab6e1b7551089608375fe9d266601b9cb7ac
naeginn <send.to.naegin@gmail.com>
2021-07-17 19:49:54 -0300

    - Laço While/Loop While
    01:24:24

--- files/js/aula_19.js
+++ files/js/aula_19.js
@@ -14,12 +14,21 @@
 
 // for (a, b, c)
 
-// a = Definir um variavel. (let i = 0; )
+// a = Definir um variavel pro contador. (let i = 0; )
 // b = Definir uma condição para que o loop continue. (i < 5) - (i <= 5)
 // c = Definir adicionar uma condição de incremento (i++) decremento (i--)
 
-
-
 for (let i = 1; i <= 5; i++){
-	console.log('Estou aprendendo.', i);
+	console.log('[For] Estou aprendendo.', i);
+}
+
+// 2° - While
+
+// Você precisa definir a variavel pro contador fora.
+
+let i = 5;
+
+while (i >= 1) {
+	console.log('[While] Estou aprendendo.', i);
+	i--;
 }

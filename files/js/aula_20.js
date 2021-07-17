e956acfc92cde8f255df06299172991166033263
naeginn <send.to.naegin@gmail.com>
2021-07-17 20:57:48 -0300

    - Exercicio Javascript 02
    01:35:34

---
+++ files/js/aula_20.js
@@ -0,0 +1,14 @@
+
+
+function numeroMaior(numeroUm, numeroDois){
+	if (numeroUm > numeroDois){
+		return numeroUm;
+	} else {
+	   return numeroDois;
+
+	}
+};
+
+
+let maiorNumero = numeroMaior(6, 2);
+console.log(maiorNumero)

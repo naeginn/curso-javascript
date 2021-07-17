559f14a3b048ee2600b28d5f243a16d331d60f7a
naeginn <send.to.naegin@gmail.com>
2021-07-17 20:45:28 -0300

    - For..In/Loop For..In
    01:28:18

--- files/js/aula_19.js
+++ files/js/aula_19.js
@@ -25,6 +25,7 @@ for (let i = 1; i <= 5; i++){
 // 2° - While
 
 // Você precisa definir a variavel pro contador fora.
+// Verifica primeiro e depois executa.
 
 let i = 5;
 
@@ -34,6 +35,8 @@ while (i >= 1) {
 }
 
 // 3° - Do..while
+// Você precisa definir a variavel pro contador fora.
+// Executa uma vez e verifica depois
 
 let ii = 1;
 
@@ -41,3 +44,22 @@ do {
 	console.log('[Do..While] Estou aprendendo.', ii)
 	ii++;
 } while (ii <= 5)
+
+
+// 4° - For..In
+
+// Você usa quando for pra propriedade de um objeto ou um array.
+
+const pessoa = {
+	name:'naegin',
+	idade: 25
+}
+for (let key in pessoa){
+	console.log(key, pessoa.name);
+}
+
+const cores = ['verde', 'azul', 'amarelo'];
+
+for (let i in cores){
+	console.log(i, cores[i]);
+}
